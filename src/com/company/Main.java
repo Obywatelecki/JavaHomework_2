fcc5780d69e98752ad47b2a38909705b6a46d01f
Obywatelecki <tpanecki@gmail.com>
2018-03-02 12:18:04 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,61 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Observable;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        File f = new File("D:\\Java\\JavaHomework_2\\python.html");
+
+        try (
+                FileReader fr = new FileReader(f);
+                BufferedReader bfr = new BufferedReader(fr)
+        ) {
+
+            String htmlPage = null;
+            StringBuilder sb = new StringBuilder();
+
+            while ((htmlPage = bfr.readLine()) != null) {
+                sb.append(htmlPage);
+            }
+
+            htmlPage = sb.toString().replaceAll("<[^>]*>", " ");
+            String htmlWords[] = htmlPage.split(" ");
+
+            Map<String, Integer> occurrences = new HashMap<>();
+
+            for (String word : htmlWords) {
+                if (word.length() > 3) {
+                    Integer oldCount = occurrences.get(word);
+                    if (oldCount == null) {
+                        oldCount = 0;
+                    }
+                    occurrences.put(word, oldCount + 1);
+                }
+            }
+
+            for (String word : occurrences.keySet()) {
+                System.out.println(word + " " + occurrences.get(word));
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
